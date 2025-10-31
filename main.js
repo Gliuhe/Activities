1ba62f555debbcc62f5e0119e6392974bd34a7a0
Gliuhe <129869949+Gliuhe@users.noreply.github.com>
2025-10-31 19:21:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -19,42 +19,6 @@ function Find()
 		d.insertAdjacentHTML('beforeend', '<p>第'+num+'次输出：</p>');
 		d.insertAdjacentHTML('beforeend', '<a href="https://gliuhe.github.io/Activities/AP-1.jpg" style="color: green;">点击进入（旅游征文图片1）</a>');
 	}
-	else if (a == "星期二" || a== "Tuesday")
-	{
-		d.insertAdjacentHTML('beforeend', '<br>');
-		d.insertAdjacentHTML('beforeend', '<p>第'+num+'次输出：</p>');
-		d.insertAdjacentHTML('beforeend', '<a href="https://gliuhe.github.io/Tuesday" style="color: green;">点击进入（特殊文件查询）</a>');
-	}
-	else if (a == "我想你")
-	{
-		d.insertAdjacentHTML('beforeend', '<br>');
-		d.insertAdjacentHTML('beforeend', '<p>第'+num+'次输出：</p>');
-		d.insertAdjacentHTML('beforeend', '<a href="https://gliuhe.github.io/Tuesday/fs.html" style="color: green;">:P</a>');
-	}
-	else if (a == "我喜欢你" || a== "I like you forever")
-	{
-		d.insertAdjacentHTML('beforeend', '<br>');
-		d.insertAdjacentHTML('beforeend', '<p>第'+num+'次输出：</p>');
-		d.insertAdjacentHTML('beforeend', '<p>检索失败【【【【【【】】】】】】】】】】【【【【【【【</p>');
-	}
-	else if (a == "谢谢你" || a== "谢谢")
-	{
-		d.insertAdjacentHTML('beforeend', '<br>');
-		d.insertAdjacentHTML('beforeend', '<p>第'+num+'次输出：</p>');
-		d.insertAdjacentHTML('beforeend', '<h4 style="color: yellow">谢谢的英文是：Thanks / Thank you');
-	}
-	else if (a == "Liuhe" || a== "鹨鹖"||a=="姚子熙"||a=="LiuH")
-	{
-		d.insertAdjacentHTML('beforeend', '<br>');
-		d.insertAdjacentHTML('beforeend', '<p>第'+num+'次输出：</p>');
-		d.insertAdjacentHTML('beforeend', '<p style="color:red">？？</p>');
-	}
-	else if (a == "没角色" || a== "星琪儿" ||a=="对不起"||a=="1"||a=="杨婉婷")
-	{
-		d.insertAdjacentHTML('beforeend', '<br>');
-		d.insertAdjacentHTML('beforeend', '<p>第'+num+'次输出：</p>');
-		d.insertAdjacentHTML('beforeend', '<h4 style="color:red">Error: 未检索到内容。</h4>');
-	}
 	else if (a == "AP2")
 	{
 		d.insertAdjacentHTML('beforeend', '<br>');
@@ -104,6 +68,6 @@ function Find()
 	else
 	{
 		d.insertAdjacentHTML('beforeend', '<p>第'+num+'次输出：</p>');
-		d.insertAdjacentHTML('beforeend', '<p style="color: red;">未知错误</p>');
+		d.insertAdjacentHTML('beforeend', '<p style="color: red;">你查找的内容不存在或已删除</p>');
 	}
 }
